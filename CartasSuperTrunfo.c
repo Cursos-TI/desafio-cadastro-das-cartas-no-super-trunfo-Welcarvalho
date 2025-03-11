9edb57cc8de3bcb8a5b8b28e578101fab83c2028
welcarvalho <wes_carvalho@hotmail.com>
2025-03-10 22:59:50 -0300

    Foi adicionadas 4 variaveis float para realização de operação matematica, criação de calculo em cod C e aplicação de casting, fooi adicionado mais duas perguntas a caixa de cada carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+
 
 int main() {
-    // Organização das Var dividias em conjunto 1 e 2
+    //D1: Organização das Var dividias em conjunto 1 e 2
+    //D2: Criação das float para operação matematica densidPop e perCap 
     char    estado1 , estado2;
     char    codCard1[4],  codCard2[4];
     char    namecid1[20], namecid2[20];
     int     populacao1, populacao2;
     float   area1, area2;
     float   pib1, pib2;
-    int     numTuriste1, numTuriste2; 
+    int     numTuriste1, numTuriste2;
+    float   densidPop1, densidPop2;
+    float   perCap1, perCap2;
     
-    //Apresentação do Jogo
+    
+    //D1: Apresentação do Jogo
     printf("**Olá sejá bem vindo**\n-----------------------\n SUPER TRUNFO - CIDADES\n-----------------------\n\n");
     
-    //Escolha da Carta 1:
+    //D1: Escolha da Carta 1:
     printf("-->CARTA N1<--\n");
     printf("Digite uma letra de A a H representando um dos estados\n");
     scanf(" %c", &estado1);
@@ -36,7 +36,7 @@ int main() {
     printf("Informe a quantidade de Pontos Turísticos:\n");
     scanf("%d", &numTuriste1);
 
-    //Escolha da Carta 2:
+    //D1: Escolha da Carta 2:
     printf("\n\n-->CARTA N2<--\n");
     printf("Digite uma letra de A a H representando um dos estados\n");
     scanf(" %c", &estado2);
@@ -53,25 +53,37 @@ int main() {
     printf("Informe a quantidade de Pontos Turísticos:\n");
     scanf("%d", &numTuriste2);
 
-    //Respostas CartaN1
+    //D2: Implementado cod para calcular a D. populacional e  PIB per capita.
+    densidPop1 = (float) (populacao1 /area1);
+    densidPop2 = (float) (populacao2 / area2);
+    perCap1 = (float) (pib1 / populacao1);
+    perCap2 = (float) (pib2 / populacao2);
+
+    //D1: Respostas CartaN1
+    //D2: Nova pergnta sobre Densidade Pop e Pip per capita 
     printf("\n\nCARTA N1\n"); 
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codCard1);
     printf("Nome da cidade: %s\n", namecid1);
     printf("População: %d\n", populacao1);
-    printf("Área (em km²): %2f km²\n", area1);
-    printf("PIB: %2f bilhões de reais\n", pib1);
+    printf("Área (em km²): %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", numTuriste1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidPop1);
+    printf("PIB per Capita: %.2f reais\n", perCap1);
 
-    //Respostas CartaN2
+    //D1: Respostas CartaN2
+    //D2: Nova pergnta sobre Densidade Pop e Pip per capita
     printf("\n\n\nCARTA N2\n"); 
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codCard2);
     printf("Nome da cidade: %s\n", namecid2);
     printf("População: %d\n", populacao2);
-    printf("Área (em km²): %2f km²\n", area2);
-    printf("PIB: %2f bilhões de reais\n", pib2);
+    printf("Área (em km²): %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", numTuriste2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidPop2);
+    printf("PIB per Capita: %.2f reais\n", perCap2);
 
     return 0;
 }
